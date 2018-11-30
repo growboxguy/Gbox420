9b5de116a0d235ee0dca9b02978ac5878a1178be
GrowBoxGuy <growboxguy@gmail.com>
2018-11-30 23:34:34 +0100

    Serial communication test sketch

--- Test-SerialCommunication/Test-SerialCommunication.ino
+++ Test-SerialCommunication/Test-SerialCommunication.ino
@@ -1,11 +1,9 @@
-void setup() {
-  // put your setup code here, to run once:
+void setup() {  // put your setup code here, to run once:
   Serial.begin(115200);    //2560mega console output
   Serial3.begin(115200);  //wifi console output
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop() {  // put your main code here, to run repeatedly:
   Serial.println("Marko");
   Serial3.println("Polo");
   delay(5000); 
