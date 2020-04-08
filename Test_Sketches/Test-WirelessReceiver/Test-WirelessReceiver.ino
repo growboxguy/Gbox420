3c7122a1bbb3b213b1f900534448c3ec00158cb9
growboxguy <growboxguy@gmail.com>
2020-04-09 00:19:34 +0200

    Preparing wireless communication - Part2

--- Test_Sketches/Test-WirelessReceiver/Test-WirelessReceiver.ino
+++ Test_Sketches/Test-WirelessReceiver/Test-WirelessReceiver.ino
@@ -1,6 +1,6 @@
-
-
-// SimpleRxAckPayload- the slave or the receiver
+//GrowBoxGuy - http://sites.google.com/site/growboxguy/
+//Sketch for testing: Receiving a control command using Arduino RF-Nano and Acknowledging it with a custom message
+//Based on: https://forum.arduino.cc/index.php?topic=421081.0
 
 #include <SPI.h>
 #include <nRF24L01.h>
@@ -9,73 +9,83 @@
 #define CE_PIN  10
 #define CSN_PIN 9
 
-const byte thisSlaveAddress[5] = {'R','x','A','A','A'};
-
+const byte ChannelAddress[5] = {"Hemp1"};
 RF24 radio(CE_PIN, CSN_PIN);
 
-char dataReceived[10]; // this must match dataToSend in the TX
-int ackData[2] = {109, -4000}; // the two values to be sent to the master
-bool newData = false;
+struct commandTemplate  //Max 32bytes. Template of the command sent by the Transmitter. Both Transmitter and Receiver needs to know this structure
+{
+   bool pump1Enabled; 
+   bool pump2Enabled;
+   bool pump1Stop; 
+   bool pump2Stop;
+   bool bucket1StartWatering; 
+   bool bucket2StartWatering;
+   float bucket1StartWeight;
+   float bucket1StopWeight;
+   float bucket2StartWeight;
+   float bucket2StopWeight;
+};
+struct commandTemplate ReceivedCommand;  //Variable where the actual command values will get stored
 
-//==============
+struct responseTemplate  //Max 32bytes. Template of the response sent back to the Transmitter. Both Transmitter and Receiver needs to know this structure
+{
+   bool pump1Enabled; 
+   bool pump2Enabled;
+   bool pump1State; 
+   bool pump2State;
+   float bucket1Weight;
+   float bucket2Weight;
+};
+struct responseTemplate FakeResponse = {1,1,0,1,4.20,1.23};  //Fake response sent back in the Acknowledgement after receiving a command from the Transmitter
 
 void setup() {
-
     Serial.begin(115200);
-
-    Serial.println("SimpleRxAckPayload Starting");
+    Serial.println(F("Listening..."));
     radio.begin();
     radio.setDataRate( RF24_250KBPS );
-    radio.openReadingPipe(1, thisSlaveAddress);
-
+    radio.openReadingPipe(1, ChannelAddress);
     radio.enableAckPayload();
-    
+    updateReplyData();
     radio.startListening();
-
-    radio.writeAckPayload(1, &ackData, sizeof(ackData)); // pre-load data
 }
-
-//==========
 
 void loop() {
     getData();
-    showData();
 }
-
-//============
 
 void getData() {
     if ( radio.available() ) {
-        radio.read( &dataReceived, sizeof(dataReceived) );
-        updateReplyData();
-        newData = true;
-    }
-}
-
-//================
-
-void showData() {
-    if (newData == true) {
-        Serial.print("Data received ");
-        Serial.println(dataReceived);
-        Serial.print(" ackPayload sent ");
-        Serial.print(ackData[0]);
+        radio.read( &ReceivedCommand, sizeof(ReceivedCommand) );        
+        Serial.print(F("Command received ["));
+        Serial.print(sizeof(ReceivedCommand));
+        Serial.println(F(" bytes], AckPayload sent")); 
+        Serial.print(ReceivedCommand.pump1Enabled);
         Serial.print(", ");
-        Serial.println(ackData[1]);
-        newData = false;
+        Serial.print(ReceivedCommand.pump2Enabled);
+        Serial.print(", ");
+        Serial.print(ReceivedCommand.pump1Stop);
+        Serial.print(", ");
+        Serial.print(ReceivedCommand.pump2Stop);
+        Serial.print(", ");
+        Serial.print(ReceivedCommand.bucket1StartWatering);
+        Serial.print(", ");
+        Serial.print(ReceivedCommand.bucket2StartWatering);
+        Serial.print(", ");
+        Serial.print(ReceivedCommand.bucket1StartWeight);
+        Serial.print(", ");
+        Serial.print(ReceivedCommand.bucket1StopWeight);
+        Serial.print(", ");
+        Serial.print(ReceivedCommand.bucket2StartWeight);
+        Serial.print(", ");
+        Serial.println(ReceivedCommand.bucket2StopWeight);
+        Serial.println();
+
+        updateReplyData();
     }
 }
 
-//================
-
-void updateReplyData() {
-    ackData[0] -= 1;
-    ackData[1] -= 1;
-    if (ackData[0] < 100) {
-        ackData[0] = 109;
-    }
-    if (ackData[1] < -4009) {
-        ackData[1] = -4000;
-    }
-    radio.writeAckPayload(1, &ackData, sizeof(ackData)); // load the payload for the next time
+void updateReplyData() { // so you can see that new data is being sent
+    FakeResponse.bucket1Weight = random(400, 500) / 100.0;
+    FakeResponse.bucket2Weight = random(400, 500) / 100.0;
+    radio.writeAckPayload(1, &FakeResponse, sizeof(FakeResponse)); // load the payload for the next time
 }
--- Test_Sketches/Test-WirelessTransmitter/Test-WirelessTransmitter.ino
+++ Test_Sketches/Test-WirelessTransmitter/Test-WirelessTransmitter.ino
@@ -1,96 +1,107 @@
-
-
-// SimpleTxAckPayload - the master or the transmitter
+//GrowBoxGuy - http://sites.google.com/site/growboxguy/
+//Sketch for testing: Sending a control command to an Arduino RF-Nano and reading the Acknowledgement response message
+//Based on: https://forum.arduino.cc/index.php?topic=421081.0
 
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
 
-
 #define CE_PIN  10
 #define CSN_PIN 9
 
-const byte slaveAddress[5] = {'R','x','A','A','A'};
-
+const byte ChannelAddress[5] = {"Hemp1"};
 RF24 radio(CE_PIN, CSN_PIN);
 
-char dataToSend[10] = "Message 0";
-char txNum = '0';
-int ackData[2] = {-1, -1}; // to hold the two values coming from the slave
+struct commandTemplate  //Max 32bytes. Template of the command sent to the Receiver. Both Transmitter and Receiver needs to know this structure
+{
+   bool pump1Enabled; 
+   bool pump2Enabled;
+   bool pump1Stop; 
+   bool pump2Stop;
+   bool bucket1StartWatering; 
+   bool bucket2StartWatering;
+   float bucket1StartWeight;
+   float bucket1StopWeight;
+   float bucket2StartWeight;
+   float bucket2StopWeight;
+};
+struct commandTemplate FakeCommand = {1,1,0,0,1,0,3.9,5.0,3.8,4.9};  //Fake commands sent to the Receiver
 
+
+struct responseTemplate  //Max 32bytes. Template of the response back from the Receiver. Both Transmitter and Receiver needs to know this structure
+{
+   bool pump1Enabled; 
+   bool pump2Enabled;
+   bool pump1State; 
+   bool pump2State;
+   float bucket1Weight;
+   float bucket2Weight;
+};
+struct responseTemplate AckResponse; //The response from the Receiver will be stored here, represents the current status of the Receiver
 
 unsigned long currentMillis;
 unsigned long prevMillis;
 unsigned long txIntervalMillis = 1000; // send once per second
 
-//===============
-
 void setup() {
-
     Serial.begin(115200);
-    //Serial.println(F("Source File /mnt/sdb1/SGT-Prog/Arduino/ForumDemos/nRF24Tutorial/SimpleTxAckPayload.ino"));
-    Serial.println("SimpleTxAckPayload Starting");
-
+    Serial.println(F("Sending and waiting for ACK"));
     radio.begin();
     radio.setDataRate( RF24_250KBPS );
-
     radio.enableAckPayload();
-
-    radio.setRetries(5,5); // delay, count
-                                       // 5 gives a 1500 µsec delay which is needed for a 32 byte ackPayload
-    radio.openWritingPipe(slaveAddress);
+    radio.setRetries(5,5); // delay, count. 5 gives a 1500 µsec delay which is needed for a 32 byte ackPayload
+    radio.openWritingPipe(ChannelAddress);
 }
 
-//=============
-
 void loop() {
-
     currentMillis = millis();
     if (currentMillis - prevMillis >= txIntervalMillis) {
         send();
     }
 }
 
-//================
-
 void send() {
-
     bool rslt;
-    rslt = radio.write( &dataToSend, sizeof(dataToSend) );
+    rslt = radio.write( &FakeCommand, sizeof(FakeCommand) );
         // Always use sizeof() as it gives the size as the number of bytes.
         // For example if dataToSend was an int sizeof() would correctly return 2
 
-    Serial.print("Data Sent ");
-    Serial.print(dataToSend);
+    Serial.print(F("Data Sent."));
     if (rslt) {
         if ( radio.isAckPayloadAvailable() ) {
-            radio.read(&ackData, sizeof(ackData));
-            Serial.print("  Acknowledge data ");
-            Serial.print(ackData[0]);
+            radio.read(&AckResponse, sizeof(AckResponse));
+            Serial.print(F(" Acknowledgement received[ "));            
+            Serial.print(sizeof(AckResponse));
+            Serial.println(F(" bytes]"));
+            Serial.print(AckResponse.pump1Enabled);
             Serial.print(", ");
-            Serial.println(ackData[1]);
+            Serial.print(AckResponse.pump2Enabled);
+            Serial.print(", ");
+            Serial.print(AckResponse.pump1State);
+            Serial.print(", ");
+            Serial.print(AckResponse.pump2State);
+            Serial.print(", ");
+            Serial.print(AckResponse.bucket1Weight);
+            Serial.print(", ");
+            Serial.println(AckResponse.bucket2Weight);
             Serial.println();
             updateMessage();
         }
         else {
-            Serial.println("  Acknowledge but no data ");
+            Serial.println(F(" Acknowledgement received without any data."));
         }        
     }
     else {
-        Serial.println("  Tx failed");
+        Serial.println(F(" No response."));
     }
-
     prevMillis = millis();
  }
 
-
-//================
-
-void updateMessage() {
-        // so you can see that new data is being sent
-    txNum += 1;
-    if (txNum > '9') {
-        txNum = '0';
-    }
-    dataToSend[8] = txNum;
+void updateMessage() {        // so you can see that new data is being sent
+    FakeCommand.pump1Enabled = random(0,2);  //Generate random bool: 0 or 1. The max limit is exlusive!
+    FakeCommand.pump2Enabled = random(0,2);
+    FakeCommand.pump1Stop = random(0,2); 
+    FakeCommand.pump2Stop = random(0,2);
+    FakeCommand.bucket1StartWatering = random(0,2); 
+    FakeCommand.bucket2StartWatering = random(0,2);
 }
