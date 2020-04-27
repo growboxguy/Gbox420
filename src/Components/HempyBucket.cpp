00f5d3a5074cf5c63bb44d96f1009413229c14a8
growboxguy <growboxguy@gmail.com>
2020-04-27 23:40:46 +0200

    Add Hempy bucket components to Mega

--- src/Components/HempyBucket.cpp
+++ src/Components/HempyBucket.cpp
@@ -1,17 +1,15 @@
 #include "HempyBucket.h"
 
-HempyBucket::HempyBucket(const __FlashStringHelper *Name, Module *Parent, Settings::HempyBucketSettings *DefaultSettings,WeightSensor *FeedbackWeightSensor) : Common(Name)
+HempyBucket::HempyBucket(const __FlashStringHelper *Name, Module *Parent, Settings::HempyBucketSettings *DefaultSettings,WeightSensor *BucketWeightSensor, WaterPump *BucketPump) : Common(Name)
 {
   this->Parent = Parent;
-  this->FeedbackWeightSensor = FeedbackWeightSensor;
-  PumpPin = &DefaultSettings->PumpPin;
-  PumpEnabled = &DefaultSettings->PumpEnabled;
-  PumpTimeout= &DefaultSettings->PumpTimeout;
+  this->BucketWeightSensor = BucketWeightSensor;
+  this->BucketPump = BucketPump;
   StartWeight = &DefaultSettings->StartWeight;
   StopWeight = &DefaultSettings->StopWeight;
   Parent->addToReportQueue(this);         ///Subscribing to the report queue: Calls the report() method
-  Parent->addToRefreshQueue_Minute(this); ///Subscribing to the 1 minute refresh queue: Calls the refresh_Minute() method
   Parent->addToRefreshQueue_Sec(this);  
+  Parent->addToRefreshQueue_Minute(this); ///Subscribing to the 1 minute refresh queue: Calls the refresh_Minute() method
   logToSerials(F("Hempy bucket object created"), true, 1);
 }
 
@@ -24,79 +22,34 @@ void HempyBucket::refresh_Minute()
 
 void HempyBucket::refresh_Sec()
 {
-  refresh_Minute(); ///temporary for faster testing
-  if(PumpOn) checkBucketWeight(); ///When watering check if its time to stop
+ if(BucketPump -> getOnState()) checkBucketWeight(); ///When watering check if its time to stop
 }
 
 void HempyBucket::checkBucketWeight()
 {  
-  if(PumpOn)
-  {    
-    if (millis() - PumpTimer > ((uint32_t)*PumpTimeout * 1000))
-    {
-      pumpOff(false);
-    }
-    else
-    {  
-      FeedbackWeightSensor -> readWeight();  //Force a weight refresh   
-      if(FeedbackWeightSensor -> getWeight() > *StopWeight)
-      {
-        pumpOff(false);
-      }
-    }
-  }
-  else  ///If pump is off
+  BucketWeightSensor -> readWeight();  //Force a weight refresh   
+  if(BucketWeightSensor -> getWeight() > *StopWeight && BucketPump -> getOnState()) ///If the weight is over the limit and the pump is on
   {
-     if(FeedbackWeightSensor -> getWeight() < *StartWeight)
-      {
-        waterNow(false);
-      }
+    BucketPump -> turnOff();     
   }
-  
+  else if(BucketWeightSensor -> getWeight() < *StartWeight && !BucketPump -> getOnState())  ///If the weight is below the limit and the pump is off
+  {
+    BucketPump -> turnOn(); 
+  }   
 }
 
 void HempyBucket::report()
 {
   Common::report();
   memset(&LongMessage[0], 0, sizeof(LongMessage)); ///clear variable
-  strcat_P(LongMessage, (PGM_P)F("Pump:"));
-  strcat(LongMessage, onOffToText(PumpOn));
-  strcat_P(LongMessage, (PGM_P)F(" ; Reservoir:"));
-  strcat_P(LongMessage, (PGM_P)F("100%")); ///Fake value \todo Integrate ultrasonic depth sensor 
+  strcat_P(LongMessage, (PGM_P)F("Weight:"));
+  strcat(LongMessage, BucketWeightSensor -> getWeightText(true));
+  strcat_P(LongMessage, (PGM_P)F(" ["));
+  strcat(LongMessage, toText(*StartWeight));
+  strcat_P(LongMessage, (PGM_P)F("/"));
+  strcat(LongMessage, toText(*StopWeight));
+  strcat_P(LongMessage, (PGM_P)F("]"));
   logToSerials(&LongMessage, true, 1);
-}
-
-void HempyBucket::waterNow(bool UserRequest)
-{
-  if (UserRequest)
-  { ///Provide feedback if the pump was turned on by the user, not by the automation
-    Parent->addToLog(F("Watering..."));
-    Parent->getSoundObject()->playOnSound();
-  }
-  if(FeedbackWeightSensor -> getWeight() < *StopWeight) {
-    PumpTimer = millis();
-    PumpOn = true;
-    checkRelay();
-  }
-}
-
-void HempyBucket::pumpOff(bool UserRequest)
-{
-  if (UserRequest)
-  { ///Provide feedback if the pump was turned off by the user, not by the automation
-    Parent->addToLog(F("Pump off"));
-    Parent->getSoundObject()->playOffSound();
-  }
-  PumpOn = false;
-  checkRelay();
-}
-
-void HempyBucket::checkRelay()
-{
-  if (PumpOn)
-    digitalWrite(*PumpPin, LOW);
-  else
-    digitalWrite(*PumpPin, HIGH);  
 }
 
 char *HempyBucket::getStartWeightText(bool IncludeUnits)
--- src/Components/HempyBucket.h
+++ src/Components/HempyBucket.h
@@ -3,6 +3,7 @@
 #include "420Module.h"
 #include "Sound.h"
 #include "WeightSensor.h"
+#include "WaterPump.h"
 
 ///UNDER DEVELOPMENT - Class representing a single hempy Bucket
 ///Weight sensor measures the bucket weight and watering is based on the measured weight
@@ -10,12 +11,12 @@
 class HempyBucket : virtual public Common
 {
 public:
-  HempyBucket(const __FlashStringHelper *Name, Module *Parent, Settings::HempyBucketSettings *DefaultSettings, WeightSensor *FeedbackWeightSensor);
+  HempyBucket(const __FlashStringHelper *Name, Module *Parent, Settings::HempyBucketSettings *DefaultSettings, WeightSensor *BucketWeightSensor, WaterPump *BucketPump);
   void refresh_Minute();
   void refresh_Sec();
   void report();
-  void waterNow(bool UserRequest);  ///Turn on water pump, run until StopWeight is reached
-  void pumpOff(bool UserRequest);  ///Turn the pump off
+  void waterNow(bool UserRequest = false);  ///Turn on water pump, run until StopWeight is reached
+  
   void checkRelay(); ///Sets the relay controlling the pump
   void checkBucketWeight();
   void setStartWeight(float Weight);
@@ -27,12 +28,8 @@ private:
 
 protected:
   Module *Parent;
-  WeightSensor *FeedbackWeightSensor;  ///Weight sensor to monitor the Hempy Bucket's weight, used to figure out when to start and stop watering
-  byte *PumpPin;
-  bool *PumpEnabled;  ///Enable/disable watering based on weight
-  bool PumpOn = false;  
-  int *PumpTimeout;  ///Max pump run time in seconds
-  uint32_t PumpTimer = millis();  ///Used for Pump runtime tracking
+  WeightSensor *BucketWeightSensor;  ///Weight sensor to monitor the Hempy Bucket's weight, used to figure out when to start and stop watering
+  WaterPump *BucketPump;  ///Weight sensor to monitor the Hempy Bucket's weight, used to figure out when to start and stop watering
   float *StartWeight;  ///Start watering below this weight
   float *StopWeight; ///Stop watering above this weight
 };
