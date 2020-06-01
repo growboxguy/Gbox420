aa7cec407e6bb3dfdffd889fc3dc18eef7fa36c8
GrowBoxGuy <growboxguy@gmail.com>
2020-06-01 17:06:57 +0200

    Using Column numbers as variables- Part 2

--- GoogleAppScript/Status.js
+++ GoogleAppScript/Status.js
@@ -1,3 +1,6 @@
+var status_keyColumn = 0;
+var status_valueColumn = 1;
+
 function Test_UpdateStatus() {
     FakeJSONData = JSON.parse(SpreadsheetApp.getActive().getRangeByName("LastReportJSON").getDisplayValue());
     UpdateStatus(FakeJSONData.Log);
@@ -11,7 +14,7 @@ function UpdateStatus(Log) {
   });
   var newStatus = [];
   for (var i = 0; i < status.length; i++) {
-    newStatus.push([status[i][0], null]); //pre-fill with null values
+    newStatus.push([status[i][status_keyColumn], null]); //pre-fill with null values
   }
   //Add all Key + Data pairs to the Status sheet from the received Log JSON  
   var Components = Object.getOwnPropertyNames(Log);
@@ -25,14 +28,14 @@ function UpdateStatus(Log) {
                           var valueToWriteBack = GetFriendlyValue(key, value);
     var match = -1;
     for (var k = 0; k < newStatus.length; k++) {
-      if (newStatus[k][0] == key) {
+      if (newStatus[k][status_keyColumn] == key) {
         match = k; //Trying to find Status entry with the relayed component name
         break;
       }
     }
     if (match != -1) { //if a match was found
       try {
-        newStatus[match][1] = valueToWriteBack; //Update matched row's second column                    
+        newStatus[match][status_valueColumn] = valueToWriteBack; //Update matched row's second column                    
       }
       catch (e) {
         LogToConsole("Error updating status row " + key +" : "+ e, true, 1);
@@ -52,5 +55,4 @@ function UpdateStatus(Log) {
 }
 LogToConsole("Writing " + newStatus, true, 1);
 SpreadsheetApp.getActive().getSheetByName("Status").getRange(1, 1, newStatus.length, 2).setValues(newStatus);
-//SpreadsheetApp.getActive().setNamedRange("statusKeys", 'Status!A2:B');
 }
