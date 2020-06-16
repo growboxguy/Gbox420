1cd0ab363b4d0a11fd89a32f278a27cb86e3b6ae
growboxguy <growboxguy@gmail.com>
2020-06-17 00:03:11 +0200

    8 port relay test sketch

--- Test_Sketches/Test-8PortRelay/Test-8PortRelay.ino
+++ Test_Sketches/Test-8PortRelay/Test-8PortRelay.ino
@@ -14,6 +14,8 @@ const byte Relay8OutPin = 29; //Power relay Port 8
 void setup()
 { // put your setup code here, to run once:
     Serial.begin(115200);
+    Serial.println(F("Sketch for testing: 8 channel relay module"));
+
     pinMode(Relay1OutPin, OUTPUT);
     pinMode(Relay2OutPin, OUTPUT);
     pinMode(Relay3OutPin, OUTPUT);
@@ -23,6 +25,7 @@ void setup()
     pinMode(Relay7OutPin, OUTPUT);
     pinMode(Relay8OutPin, OUTPUT);
 
+    //Setting all relays off
     digitalWrite(Relay1OutPin, HIGH);
     digitalWrite(Relay2OutPin, HIGH);
     digitalWrite(Relay3OutPin, HIGH);
@@ -32,63 +35,63 @@ void setup()
     digitalWrite(Relay7OutPin, HIGH);
     digitalWrite(Relay8OutPin, HIGH);
 
-    delay(5000);
+    delay(2000);
     Serial.println(F("Turning Relay1 ON"));
     digitalWrite(Relay1OutPin, LOW);
-    delay(5000);
+    delay(2000);
     Serial.println(F("Turning Relay2 ON"));
     digitalWrite(Relay2OutPin, LOW);
-    delay(5000);
+    delay(2000);
     Serial.println(F("Turning Relay3 ON"));
     digitalWrite(Relay3OutPin, LOW);
-    delay(5000);
+    delay(2000);
     Serial.println(F("Turning Relay4 ON")); //NOTE: The relay module will not get enough power after turning on the 4th relay if it is powered from the Arduino
     digitalWrite(Relay4OutPin, LOW);        //To operate a relay on 5V you need 60mA per relay. Arduino Mega can only supply 200mA max on VCC, limiting the number of simultaneously active relays to 3.
-    delay(5000);
+    delay(2000);
     Serial.println(F("Turning Relay5 ON"));
     digitalWrite(Relay5OutPin, LOW);
-    delay(5000);
+    delay(2000);
     Serial.println(F("Turning Relay6 ON"));
     digitalWrite(Relay6OutPin, LOW);
-    delay(5000);
+    delay(2000);
     Serial.println(F("Turning Relay7 ON"));
     digitalWrite(Relay7OutPin, LOW);
-    delay(5000);
+    delay(2000);
     Serial.println(F("Turning Relay8 ON"));
     digitalWrite(Relay8OutPin, LOW);
 }
 
 void loop()
 { // put your main code here, to run repeatedly:
-    delay(5000);
+    delay(2000);
     Serial.println(F("Turning Relay1 ON"));
     digitalWrite(Relay8OutPin, HIGH);
     digitalWrite(Relay1OutPin, LOW);
-    delay(5000);
+    delay(2000);
     Serial.println(F("Turning Relay2 ON"));
     digitalWrite(Relay1OutPin, HIGH);
     digitalWrite(Relay2OutPin, LOW);
-    delay(5000);
+    delay(2000);
     Serial.println(F("Turning Relay3 ON"));
     digitalWrite(Relay2OutPin, HIGH);
     digitalWrite(Relay3OutPin, LOW);
-    delay(5000);
+    delay(2000);
     Serial.println(F("Turning Relay4 ON"));
     digitalWrite(Relay3OutPin, HIGH);
     digitalWrite(Relay4OutPin, LOW);
-    delay(5000);
+    delay(2000);
     Serial.println(F("Turning Relay5 ON"));
     digitalWrite(Relay4OutPin, HIGH);
     digitalWrite(Relay5OutPin, LOW);
-    delay(5000);
+    delay(2000);
     Serial.println(F("Turning Relay6 ON"));
     digitalWrite(Relay5OutPin, HIGH);
     digitalWrite(Relay6OutPin, LOW);
-    delay(5000);
+    delay(2000);
     Serial.println(F("Turning Relay7 ON"));
     digitalWrite(Relay6OutPin, HIGH);
     digitalWrite(Relay7OutPin, LOW);
-    delay(5000);
+    delay(2000);
     Serial.println(F("Turning Relay8 ON"));
     digitalWrite(Relay7OutPin, HIGH);
     digitalWrite(Relay8OutPin, LOW);
