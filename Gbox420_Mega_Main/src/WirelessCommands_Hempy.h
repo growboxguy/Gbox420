e2f84a454da5884ebe57f6116823b79d98e65a41
growboxguy <growboxguy@gmail.com>
2020-08-13 00:19:59 +0200

    Wireless: Sequence ID added as common parent

--- Gbox420_Mega_Main/src/WirelessCommands_Hempy.h
+++ Gbox420_Mega_Main/src/WirelessCommands_Hempy.h
@@ -10,15 +10,27 @@ const uint8_t NumberOfResponses = 2; //How many 32byte Responses packages are th
 struct commonAttributes  ///< Shared between Command and Respone packages
 {
    uint8_t SequenceID;  ///< Commands and Responses can span across multiple 32byte packages. Packages with 0 SequenceID represent the initial attempt to exchange data, followup packages with increasing SequenceID are exchanged up to the defined numberOfCommands or numberOfRespone global variables.
+   
+   commonAttributes(uint8_t SequenceID){
+      this -> SequenceID = SequenceID;
+   }
+};
+
+struct hempyResponse : commonAttributes  ///Max 32 bytes. Template of the response sent back to the Transmitter. Both Transmitter and Receiver needs to know this structure
+{
+   hempyResponse() : commonAttributes(1){} 
+   PumpState PumpState_B1 = DISABLED;
+   float Weight_B1 = 0.0;
+
+   PumpState PumpState_B2 = DISABLED;
+   float Weight_B2 = 0.0;
 };
 
 struct hempyCommand : commonAttributes  ///Max 32 bytes. Template of the command sent by the Transmitter. Both Transmitter and Receiver needs to know this structure
 {   
 
 ///Structs for wireless communication
-hempyCommand() : commonAttributes(){}
-
-   SequenceID = 0;   //This is the initial package that triggers a data exchange process between modules
+   hempyCommand() : commonAttributes(2){}
    time_t Time;
    bool Debug = false;
    bool Metric = true;
@@ -41,19 +53,11 @@ hempyCommand() : commonAttributes(){}
 };
 
 struct hempyCommand_Part2 : commonAttributes  ///Max 32 bytes. Template of the command sent by the Transmitter. Both Transmitter and Receiver needs to know this structure
-{   
-   SequenceID = 1;
+{  
+   hempyCommand_Part2() : commonAttributes(1){} 
    time_t Time;
    bool Debug = false;
    bool Metric = true; 
    int Test = 420;
 };
 
-struct hempyResponse : commonAttributes  ///Max 32 bytes. Template of the response sent back to the Transmitter. Both Transmitter and Receiver needs to know this structure
-{
-   PumpState PumpState_B1 = DISABLED;
-   float Weight_B1 = 0.0;
-
-   PumpState PumpState_B2 = DISABLED;
-   float Weight_B2 = 0.0;
-};
