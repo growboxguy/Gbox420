a2b85654a0dcc38a1828c02cd168bfae8ac24102
growboxguy <growboxguy@gmail.com>
2020-12-02 00:21:43 +0100

    Misc - Image processing for the website

--- libraries/_BuildScripts/_ImageOptimizer.sh
+++ libraries/_BuildScripts/_ImageOptimizer.sh
@@ -6,7 +6,7 @@ SOURCE="./$SOURCE"
 read -p "Target folder (default: ./_Destionation): " DESTINATION
 DESTINATION=${DESTINATION:-_Destination}
 DESTINATION="./$DESTINATION"
-read -p "Tag images (Output: TAG_imgage.jpg): " TAG
+read -p "Tag file names (Output: TAG_image.jpg): " TAG
 
 if [ ! -d "$DESTINATION" ]; then
     echo "Creating $DESTINATION ..."
@@ -17,25 +17,25 @@ if [ ! -d "$DESTINATION/original" ]; then
     mkdir "$DESTINATION/original"
 fi
 
+echo "Moving source files to $DESTINATION/original..."
+ls $SOURCE | cat -n | while read n f; do mv "$SOURCE/$f" "$DESTINATION/original/${TAG}_$f"; done
+
 echo "Converting PNG files to JPG..."
-for i in $SOURCE/*.png ; do [ -f "$i" ] || continue; convert "$i" "${i%.*}.jpg" ; done
+for i in `ls $DESTINATION/original/ | grep -i .png`; do convert "$i" "${i%.*}.jpg" ; done
 
-echo "Storing original images in $DESTINATION/original :"
-ls $SOURCE| cat -n | while read n f; do cp "$SOURCE/$f" "$DESTINATION/original/${TAG}_$f"; done
-echo "Moving images to $DESTINATION :"
-ls $SOURCE| cat -n | while read n f; do cp "$SOURCE/$f" "$DESTINATION/${TAG}_$(printf "%04d" $n).jpg"; done
-#ls $SOURCE| cat -n | while read n f; do mv "$SOURCE/$f" "$DESTINATION/${TAG}_$(printf "%04d" $n).jpg"; done
+echo "Copying JPG images to $DESTINATION..."
+ls $DESTINATION/original/ | grep -i .jpg | cat -n | while read n f; do cp "$DESTINATION/original/$f" "$DESTINATION/${TAG}_$n.jpg" ; done
 
-echo "Stripping EXIF data :"
-for i in $DESTINATION/$TAG*.jpg; do echo "Processing $i"; exiftool -overwrite_original -all= "$i"; done
-for i in $DESTINATION/$TAG*.jpg; do echo "Processing $i"; exiftool -overwrite_original -geotag= "$i"; done
-#for i in ./"$DESTINATION"/*.jpg_original; do rm "$i"; done
+echo "Stripping EXIF data..."
+for i in `ls $DESTINATION/ | grep -i .jpg`; do echo "  Processing EXIF - $i"; exiftool -overwrite_original -all= "$DESTINATION/$i"; done
+#for i in $DESTINATION/$TAG*.jpg; do echo "Processing $i"; exiftool -overwrite_original -geotag= "$i"; done
 
 echo "Changing resolution..."
-for i in $DESTINATION/$TAG*.jpg; do convert $i -resize '1280x>' $i; done
+for i in `ls $DESTINATION/ | grep -i .jpg`; do echo "  Resizing - $i"; convert "$DESTINATION/$i" -resize '1280x>' "$DESTINATION/$i"; done
 
 echo "Optimising JPG..."
-jpegoptim -m 70 -s $DESTINATION/$TAG*.jpg
+jpegoptim -m 80 -s $DESTINATION/*
+
 echo
 echo "Finished. Press enter to exit."    
 read fakeinput
