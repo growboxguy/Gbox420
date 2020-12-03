05179a9b0c854fa2bbc197c3b98377051786553f
growboxguy <growboxguy@gmail.com>
2020-12-03 11:30:20 +0100

    Misc - Image processing for the website P2

--- libraries/_BuildScripts/_ImageOptimizer.sh
+++ libraries/_BuildScripts/_ImageOptimizer.sh
@@ -12,29 +12,29 @@ if [ ! -d "$DESTINATION" ]; then
     echo "Creating $DESTINATION ..."
     mkdir "$DESTINATION"
 fi
-if [ ! -d "$DESTINATION/original" ]; then
-    echo "Creating $DESTINATION/original ..."
-    mkdir "$DESTINATION/original"
+if [ ! -d "$DESTINATION/original_$TAG" ]; then
+    echo "Creating $DESTINATION/original_$TAG ..."
+    mkdir "$DESTINATION/original_$TAG"
 fi
 
-echo "Moving source files to $DESTINATION/original..."
-ls $SOURCE | cat -n | while read n f; do mv "$SOURCE/$f" "$DESTINATION/original/${TAG}_$f"; done
+echo "Storing original images in $DESTINATION/original_$TAG..."
+ls $SOURCE | cat -n | while read n f; do mv "$SOURCE/$f" "$DESTINATION/original_$TAG/${TAG}_$f"; done
 
 echo "Converting PNG files to JPG..."
-for i in `ls $DESTINATION/original/ | grep -i .png`; do convert "$i" "${i%.*}.jpg" ; done
+for i in `ls $DESTINATION/original_$TAG/ | grep -i .png`; do convert "$i" "${i%.*}.jpg" ; done
 
 echo "Copying JPG images to $DESTINATION..."
-ls $DESTINATION/original/ | grep -i .jpg | cat -n | while read n f; do cp "$DESTINATION/original/$f" "$DESTINATION/${TAG}_$n.jpg" ; done
+ls $DESTINATION/original_$TAG/ | grep -i .jpg | cat -n | while read n f; do cp "$DESTINATION/original_$TAG/$f" "$DESTINATION/${TAG}_$n.jpg" ; done
 
 echo "Stripping EXIF data..."
-for i in `ls $DESTINATION/ | grep -i .jpg`; do echo "  Processing EXIF - $i"; exiftool -overwrite_original -all= "$DESTINATION/$i"; done
+for i in `ls $DESTINATION/ | grep -i $TAG.*.jpg`; do echo "  Processing EXIF - $i"; exiftool -overwrite_original -all= "$DESTINATION/$i"; done
 #for i in $DESTINATION/$TAG*.jpg; do echo "Processing $i"; exiftool -overwrite_original -geotag= "$i"; done
 
 echo "Changing resolution..."
-for i in `ls $DESTINATION/ | grep -i .jpg`; do echo "  Resizing - $i"; convert "$DESTINATION/$i" -resize '1280x>' "$DESTINATION/$i"; done
+for i in `ls $DESTINATION/ | grep -i $TAG.*.jpg`; do echo "  Resizing - $i"; convert "$DESTINATION/$i" -resize '1280x>' "$DESTINATION/$i"; done
 
 echo "Optimising JPG..."
-jpegoptim -m 80 -s $DESTINATION/*
+jpegoptim -m 80 -s $DESTINATION/$TAG*
 
 echo
 echo "Finished. Press enter to exit."    
