9c40e43cf03f2d66029e6344130a302320e903a2
growboxguy <growboxguy@gmail.com>
2021-01-13 23:19:44 +0100

    Test PWM dimming/controlling AC devices - P1

---
+++ Test_Sketches/Test-DimmingPWM_AC/Test-DimmingPWM_AC/Test-DimmingPWM_AC.ino
@@ -0,0 +1,65 @@
+//GrowBoxGuy - http://sites.google.com/site/growboxguy/
+//Sketch for testing PWM Output/controlling of AC devices using Robodyn AC Light Dimmin Module with Zero-cross detection
+
+#include "RBDdimmer.h"
+
+//Pins
+//const byte ZC_Pin = 2;   //Zero Cross detection pin - This pin is FIXED to D2 digital pin due to Interroupt handling
+const byte PWM_Pin = 12;  //PWM signal that sets the Output
+const byte MinOutput = 35;
+const byte MaxOutput = 100;
+int Output = MaxOutput;  //Start with full power
+bool isGettingHigh = true; //Direction to increase or decrease Output
+
+dimmerLamp ACDevice(PWM_Pin);   //Any device that can be controlled adjusted using AC PWM signals (AC Motors, Incandescent light bulb)
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println(F("Sketch for testing AC PWM Output/controlling"));
+  Serial.println();
+
+  ACDevice.begin(NORMAL_MODE, OFF); //dimmer initialisation: name.begin(Mode: NORMAL_MODE/TOGGLE_MODE, State:ON/OFF) 
+  delay(5000); //Wait 5 seconds
+  Serial.println(F("Turning on device..."));
+  ACDevice.setPower(Output);
+  ACDevice.setState(ON); //Turn on device
+
+  //Mode: NORMAL_MODE to make dimmer work in defined value from 0 to 100 (%) (integer)
+  //Mode: TOGGLE_MODE smooth change of Output value up or down in a defined range. This solutions implies change of Output values by means of hardware timer, without using the cycle code.
+  //State: ON - turns timer ON, allows to use dimmer.
+  //State: OFF - turns timer parameters OFF, prevents the use of dimmer.
+}
+
+void loop()
+{
+ while (MinOutput <= Output && Output <= MaxOutput)
+  {
+    ACDevice.setPower(Output); 
+    
+    if (Output % 10 == 0)   //modulo division, https://www.arduino.cc/reference/en/language/structure/arithmetic-operators/modulo/
+    {
+      Serial.print(Output);
+      Serial.println("%");
+    }
+    if (isGettingHigh)
+      Output++;
+    else
+      Output--;
+    delay(1000); //Wait 1seconds between 1% change
+  }
+
+  //When the while loop above breaks the Output is out of the 0 to 100 bounds
+  if (isGettingHigh)
+  {
+    Serial.println("MAX Output reached");
+    Output = MaxOutput; //Set Output back within bounds
+  }
+  else
+  {
+    Serial.println("MIN Output reached");
+    Output = MinOutput;  //Set Output back within bounds
+  }
+  isGettingHigh = !isGettingHigh; // flip the direction
+  delay(5000);                    //Wait 5 seconds
+}
