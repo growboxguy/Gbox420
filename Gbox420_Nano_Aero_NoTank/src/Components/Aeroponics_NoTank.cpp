ee99842d373675070935ec0a0dd01c744e87d3bf
growboxguy <growboxguy@gmail.com>
2021-01-25 19:50:10 +0100

    NoTank - Removed inheritance

--- Gbox420_Nano_Aero_NoTank/src/Components/Aeroponics_NoTank.cpp
+++ Gbox420_Nano_Aero_NoTank/src/Components/Aeroponics_NoTank.cpp
@@ -1,12 +1,20 @@
 #include "Aeroponics_NoTank.h"
 
-Aeroponics_NoTank::Aeroponics_NoTank(const __FlashStringHelper *Name, Module *Parent, Settings::AeroponicsSettings *DefaultSettings, PressureSensor *FeedbackPressureSensor, PressurePump *Pump) : Aeroponics(Name, Parent, DefaultSettings, FeedbackPressureSensor, Pump)
+Aeroponics_NoTank::Aeroponics_NoTank(const __FlashStringHelper *Name, Module *Parent, Settings::AeroponicsSettings *DefaultSettings, PressureSensor *FeedbackPressureSensor, PressurePump *Pump) : Common(Name)
 {
-  this->Name = Name;
-  logToSerials(F(""), true, 0);  //New line
-  logToSerials(F(""), false, 1); //Extra indentation
+  this->Parent = Parent;
+  this->FeedbackPressureSensor = FeedbackPressureSensor;
+  this->Pump = Pump;
+  SprayEnabled = &DefaultSettings->SprayEnabled;   ///< Enable/disable misting
+  Duration = &DefaultSettings->Duration;     ///< Spray time in seconds
+  DayInterval = &DefaultSettings->DayInterval;     ///< Spray every X minutes - With lights ON
+  NightInterval = &DefaultSettings->NightInterval; ///< Spray every X minutes - With lights OFF
+  MaxPressure = &DefaultSettings->MaxPressure;     ///< Aeroponics - Turn off pump above this pressure (bar)
+  Parent->addToReportQueue(this);
+  Parent->addToRefreshQueue_Sec(this);
   logToSerials(F("Aeroponics_NoTank object created"), true, 1);
   sprayNow(false); ///< This is a safety feature,start with a spray after a reset
+
 }
 
 void Aeroponics_NoTank::report()
@@ -15,8 +23,21 @@ void Aeroponics_NoTank::report()
   memset(&LongMessage[0], 0, sizeof(LongMessage)); ///< clear variable
   strcat_P(LongMessage, (PGM_P)F("LastSprayPressure:"));
   strcat(LongMessage, toText_pressure(LastSprayPressure));
-  logToSerials(&LongMessage, false, 1); ///< first print Aeroponics_Tank specific report, without a line break
-  Aeroponics::report();                 ///< then print parent class report
+  strcat_P(LongMessage, (PGM_P)F(" ; MaxPressure:"));
+  strcat(LongMessage, toText_pressure(*MaxPressure));
+  strcat_P(LongMessage, (PGM_P)F(" ; SprayEnabled:"));
+  strcat(LongMessage, toText_yesNo(SprayEnabled));
+  strcat_P(LongMessage, (PGM_P)F(" ; DayMode:"));
+  strcat(LongMessage, toText_enabledDisabled(DayMode));
+  strcat_P(LongMessage, (PGM_P)F(" ; Duration:"));
+  strcat(LongMessage, toText_second(*Duration));
+  strcat_P(LongMessage, (PGM_P)F(" ; DayInterval:"));
+  strcat(LongMessage, toText_minute(*DayInterval));
+  strcat_P(LongMessage, (PGM_P)F(" ; NightInterval:"));
+  strcat(LongMessage, toText_minute(*NightInterval));
+  logToSerials(&LongMessage, true, 1); ///< Break line, No indentation needed: child class already printed it
+
+
 }
 
 void Aeroponics_NoTank::refresh_Sec()
@@ -44,7 +65,7 @@ void Aeroponics_NoTank::checkPump(bool OnlyTurnOff)
 
     if (!RunTillTimeout && (millis() - SprayTimer) >= *Duration * 1000 + (uint32_t)Pump->getPrimingTime() * 1000)
     { ///< bypass valve is closed and time to stop spraying
-      LastSprayPressure = Aeroponics::FeedbackPressureSensor->getPressure();
+      LastSprayPressure = FeedbackPressureSensor->getPressure();
       logToSerials(F("Spray finished"), false, 3);
       Pump->stopPump();
     }
@@ -96,4 +117,146 @@ void Aeroponics_NoTank::lockPumpOn()
 {
   Pump->startPump(true);
   RunTillTimeout = true;
+}
+
+
+void Aeroponics_NoTank::setDayMode(bool State)
+{
+  if (DayMode != State)
+  {
+    DayMode = State;
+    if (DayMode)
+    {
+      Parent->getSoundObject()->playOnSound();
+    }
+    else
+    {
+      Parent->getSoundObject()->playOffSound();
+    }
+  }
+}
+
+void Aeroponics_NoTank::setDayInterval(int Interval)
+{
+  if (*DayInterval != Interval && Interval > 0)
+  {
+    *DayInterval = Interval;
+    Parent->getSoundObject()->playOnSound();
+  }
+}
+
+int Aeroponics_NoTank::getDayInterval()
+{
+  return *DayInterval;
+}
+
+char *Aeroponics_NoTank::getDayIntervalText()
+{
+  return toText(*DayInterval);
+}
+
+void Aeroponics_NoTank::setNightInterval(int Interval)
+{
+  if (*NightInterval != Interval && Interval > 0)
+  {
+    *NightInterval = Interval;
+    Parent->getSoundObject()->playOnSound();
+  }
+}
+
+int Aeroponics_NoTank::getNightInterval()
+{
+  return *NightInterval;
+}
+
+char *Aeroponics_NoTank::getNightIntervalText()
+{
+  return toText(*NightInterval);
+}
+
+void Aeroponics_NoTank::setDuration(float duration)
+{
+  if (*Duration != duration && duration > 0)
+  {
+    *Duration = duration;
+    Parent->addToLog(F("Duration updated"));
+    Parent->getSoundObject()->playOnSound();
+  }
+}
+
+void Aeroponics_NoTank::setSprayOnOff(bool State)
+{
+  *SprayEnabled = State;
+  if (*SprayEnabled)
+  {
+    Parent->addToLog(F("Spray enabled"));
+    Parent->getSoundObject()->playOnSound();
+  }
+  else
+  {
+    Parent->addToLog(F("Spray disabled"));
+    Parent->getSoundObject()->playOffSound();
+  }
+}
+
+bool Aeroponics_NoTank::getSprayEnabled()
+{
+  return *SprayEnabled;
+}
+
+float Aeroponics_NoTank::getDuration()
+{
+  return *Duration;
+}
+
+char *Aeroponics_NoTank::getSprayDurationText()
+{
+  return toText(*Duration);
+}
+
+char *Aeroponics_NoTank::sprayStateToText()
+{
+  return toText_onOff(*SprayEnabled);
+}
+
+float Aeroponics_NoTank::getLastSprayPressure()
+{
+  return LastSprayPressure;
+}
+
+char *Aeroponics_NoTank::getLastSprayPressureText(bool IncludeCurrentPressure)
+{
+  memset(&ShortMessage[0], 0, sizeof(ShortMessage)); ///< clear variable
+
+  toText_pressure(LastSprayPressure); ///< loads the Last pressure measured during spraying
+  if (IncludeCurrentPressure)
+  {
+    strcat_P(ShortMessage, (PGM_P)F(" ["));
+    dtostrf(FeedbackPressureSensor->getPressure(), 4, 2, ShortMessage);
+    strcat_P(ShortMessage, (PGM_P)F("]"));
+  }
+  return ShortMessage;
+}
+
+float Aeroponics_NoTank::getPressure()
+{
+  return FeedbackPressureSensor->getPressure();
+}
+
+void Aeroponics_NoTank::setMinPressure(float Pressure)
+{
+  if (*MinPressure != Pressure && Pressure > 0)
+  {
+    *MinPressure = Pressure;
+  }
+}
+
+void Aeroponics_NoTank::setMaxPressure(float Pressure)
+{
+  if (*MaxPressure != Pressure && Pressure > 0)
+  {
+    *MaxPressure = Pressure;
+    Parent->addToLog(F("Max pressure updated"));
+    Parent->getSoundObject()->playOnSound();
+  }
 }
--- Gbox420_Nano_Aero_NoTank/src/Components/Aeroponics_NoTank.h
+++ Gbox420_Nano_Aero_NoTank/src/Components/Aeroponics_NoTank.h
@@ -1,11 +1,13 @@
 #pragma once
 
 #include "420Common.h"
-#include "Aeroponics.h"
+#include "Sound.h"
+#include "PressureSensor.h"
+#include "PressurePump.h"
 
 ///< Aeroponics tote without pressure tank
 
-class Aeroponics_NoTank : virtual public Aeroponics
+class Aeroponics_NoTank : public Common
 {
 public:
   Aeroponics_NoTank(const __FlashStringHelper *Name, Module *Parent, Settings::AeroponicsSettings *DefaultSettings, PressureSensor *FeedbackPressureSensor, PressurePump *Pump); ///< constructor
@@ -17,7 +19,40 @@ public:
   void lockPumpOn(); ///< On user request the pump can be turned ON without the spray timer turning it off.
   void processTimeCriticalStuff(); ///< Process things that cannot wait or need precise timing
 
+  PressurePump *Pump;
+
+
+  void setSprayOnOff(bool State);
+  char *sprayStateToText();
+  void setDayMode(bool State);
+  void setDuration(float Duration);
+  void setDayInterval(int Interval);
+  void setNightInterval(int Interval);
+  float getDuration();
+  int getDayInterval();
+  int getNightInterval();
+  char *getDayIntervalText();
+  char *getNightIntervalText();
+  char *getSprayDurationText();
+  bool getSprayEnabled();
+  float getPressure();
+  float getLastSprayPressure();
+  char *getLastSprayPressureText(bool IncludeCurrentPressure);
+  void setMinPressure(float Pressure);
+  void setMaxPressure(float Pressure);
+
 private:
 protected:
   bool RunTillTimeout = false; ///< While true only the Pump Timeout will stop the pump.
+  Module *Parent;
+  PressureSensor *FeedbackPressureSensor; ///< Pressure sensor object that will monitor the spray pressure
+  uint32_t SprayTimer = millis();
+  bool *SprayEnabled;          ///< Enable/disable misting
+  float *Duration;            ///< Spray time in seconds
+  int *DayInterval;            ///< Spray every X minutes - With lights ON
+  int *NightInterval;          ///< Spray every X minutes - With lights OFF
+  float *MinPressure;          ///< Minimum acceptable spray pressure
+  float *MaxPressure;          ///< Maximum allowed pressure
+  float LastSprayPressure = 0; ///< tracks the last pressure reading during a spray cycle
+  bool DayMode = true;         ///< Switch between Day and Night spray interval and duration.
 };
