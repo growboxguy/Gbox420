37d8cc41ad9b792153692b33c3db61d877015b19
growboxguy <growboxguy@gmail.com>
2021-03-28 03:56:54 +0200

    Fix stack overflow when calling snprintf

--- Gbox420_Nano_Hempy/Settings.h
+++ Gbox420_Nano_Hempy/Settings.h
@@ -14,9 +14,9 @@ static const uint8_t Version = 4; ///< Increment this after changing the stuctur
 ///< NOT SAVED TO EEPROM
 
 ///< Global constants
-static const uint8_t MaxWordLength = 22;       ///< Default char * buffer length for storing a word + null terminator. Memory intense!
-static const uint8_t MaxShotTextLength = 32;   ///< Default char * buffer length for storing mutiple words. Memory intense!
-static const uint16_t MaxLongTextLength = 256; ///< Default char * buffer length for storing a long text. Memory intense!
+static const uint8_t MaxWordLength = 32;       ///< Default char * buffer length for storing a word + null terminator. Memory intense!
+static const uint8_t MaxShotTextLength = 64;   ///< Default char * buffer length for storing mutiple words. Memory intense!
+static const uint16_t MaxLongTextLength = 192; ///< Default char * buffer length for storing a long text. Memory intense!
 static const uint8_t QueueDepth = 8;           ///< Limits the maximum number of active modules. Memory intense!
 static const uint8_t MovingAverageDepth = 8;   ///< Limits the maximum number of active modules. Memory intense!
 
