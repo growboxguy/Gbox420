5f156b77d2ea7f24676cae52340fa431f6696bbf
growboxguy <growboxguy@gmail.com>
2021-07-02 13:36:13 +0200

    UI tweaks - P2

--- Gbox420_Mega_Main/src/Components_Web/Fan_Web.cpp
+++ Gbox420_Mega_Main/src/Components_Web/Fan_Web.cpp
@@ -9,7 +9,7 @@ Fan_Web::Fan_Web(const __FlashStringHelper *Name, Module_Web *Parent, Settings::
 
 void Fan_Web::websiteEvent_Refresh(__attribute__((unused)) char *url)
 {
-  WebServer.setArgString(getName(F("S"),true), fanSpeedText());
+  WebServer.setArgString(getName(F("S"),true), fanSpeedText(true));
 }
 
 bool Fan_Web::commandEvent(__attribute__((unused)) char *Command, __attribute__((unused)) char *Data)
@@ -23,17 +23,14 @@ bool Fan_Web::commandEvent(__attribute__((unused)) char *Command, __attribute__(
     if (strcmp_P(ShortMessage, (PGM_P)F("O")) == 0)
     {
       TurnOff();
-      WebServer.setArgString(getName(F("S")), fanSpeedText(true));
     }
     else if (strcmp_P(ShortMessage, (PGM_P)F("L")) == 0)
     {
       SetLowSpeed();
-      WebServer.setArgString(getName(F("S")), fanSpeedText(true));
     }
     else if (strcmp_P(ShortMessage, (PGM_P)F("H")) == 0)
     {
       SetHighSpeed();
-      WebServer.setArgString(getName(F("S")), fanSpeedText(true));
     }
     return true;
   }
