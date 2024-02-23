ca81936ddaf4ffaf35adac4e67beff0187cfff60
GrowBoxGuy <growboxguy@gmail.com>
2024-02-23 21:25:51 +0100

    PicoW - NTP time

--- Gbox420_PicoW/Gbox420_PicoW/Gbox420_PicoW.c
+++ Gbox420_PicoW/Gbox420_PicoW/Gbox420_PicoW.c
@@ -1,8 +1,8 @@
 /*! \file
  *  \brief     Wifi connection test
- *  \details   Requires 2 parameters -DWIFI_SSID="WifiNetworkPasswordName" -DWIFI_PASSWORD="WifiNetworkPassword"
+ *  \details   
  *  \attention cd build
- *             cmake -DPICO_BOARD=pico_w -DWIFI_SSID="WifiNetworkPasswordName" -DWIFI_PASSWORD="WifiNetworkPassword" .. -G "MinGW Makefiles"
+ *             cmake .. -G "MinGW Makefiles"
  *             mingw32-make.exe
  *  \author    GrowBoxGuy  - https://sites.google.com/site/growboxguy/raspberry-pi-pico-w
  *  \version   4.20
@@ -14,10 +14,14 @@
 #include "pico/stdlib.h"
 #include "pico/cyw43_arch.h"
 #include "pico/util/datetime.h"
-// #include <string.h>
+
+#include "NTPClient.c"
+
 
 // Global constants
 #define MaxShotTextLength 128 ///< Default char * buffer length for storing mutiple words. Memory intense!
+#define WIFI_SSID "GboxNet"   //Comment out this line and pass it as a parameter to cmake -DWIFI_SSID="WifiNetworkPasswordName"
+#define WIFI_PASSWORD "SuperSecretPassword"  //Comment out this line and pass cmake -DPICO_BOARD=pico_w -DWIFI_SSID="WifiNetworkPasswordName"
 
 // Global variable initialization
 datetime_t CurrentTime;
@@ -29,7 +33,7 @@ void initializeWiFi()
     printf("Initializing WiFi...");
     if (cyw43_arch_init())
     {
-        printf("failed\n");
+        printf("...failed\n");
     }
     else
     {
@@ -37,34 +41,11 @@ void initializeWiFi()
         printf("Connecting to %s", WIFI_SSID);
         if (cyw43_arch_wifi_connect_timeout_ms(WIFI_SSID, WIFI_PASSWORD, CYW43_AUTH_WPA2_AES_PSK, 10000))
         {
-            printf("failed\n");
+            printf("...failed\n");
         }
         else
             printf("...done\n");
     }
-}
-
-// Update built-in Real Time Clock with an NTP query
-void updateRTC()
-{
-    datetime_t timeTemp = {
-        .year = 2023,
-        .month = 04,
-        .day = 20,
-        .dotw = 4, // 0 is Sunday, so 4 is Thursday
-        .hour = 16,
-        .min = 20,
-        .sec = 00};
-    rtc_set_datetime(&timeTemp);
-    printf("done\n");
-}
-
-// Initialize the built-in RTC
-void initializeRTC()
-{
-    printf("Initializing RTC...");
-    rtc_init();
-    updateRTC();
 }
 
 // Query current time from local RTC
@@ -92,20 +73,15 @@ int main()
     watchdog_enable(0x7fffff, 1);  // Maximum of 0x7fffff, which is approximately 8.3 seconds
 
     // run_ntp_test();
-    // cyw43_arch_deinit();
     int LoopCount = 0;
     absolute_time_t LastRefresh = get_absolute_time();
-    while (LoopCount < 12)
+    while (LoopCount < 10)
     {
-#if PICO_CYW43_ARCH_POLL
-        cyw43_arch_poll(); // check for WiFi driver or lwIP work that needs to be done
-        sleep_ms(1);
-#endif
         if (absolute_time_diff_us(LastRefresh, get_absolute_time()) > 5000000) // 5sec
         {
             watchdog_update();
             getRTC();
-            printf(" Listening...\n");
+            printf(" Running...\n");
             LastRefresh = get_absolute_time();
             LoopCount++;
         }
