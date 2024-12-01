da9ffe7e4abba4277a2ce07595e2f669245cc1bd
GrowBoxGuy <growboxguy@gmail.com>
2024-12-01 16:56:30 +0100

    Reduce folders included in Doxygen

--- Gbox420_Mega_Main/Settings.h
+++ Gbox420_Mega_Main/Settings.h
@@ -9,14 +9,14 @@
  *  \version   4.20
  */
 
-static const uint8_t Version = 15; ///< Increment this after changing the stucture of the SAVED TO EEPROM section to force overwriting the stored settings in the Arduino's EEPROM.
+static const uint8_t Version = 16; ///< Increment this after changing the stucture of the SAVED TO EEPROM section to force overwriting the stored settings in the Arduino's EEPROM.
 
 ///< NOT SAVED TO EEPROM
 
 ///< Global constants
 static const uint8_t MaxWordLength = 32;        ///< Default char * buffer length for storing a word + null terminator. Memory intense!
 static const uint8_t MaxShotTextLength = 128;   ///< Default char * buffer length for storing mutiple words. Memory intense!
-static const uint16_t MaxLongTextLength = 1024; ///< Default char * buffer length for storing a long text. Memory intense!
+static const uint16_t MaxLongTextLength = 1280; ///< Default char * buffer length for storing a long text. Memory intense!
 static const uint8_t LogDepth = 4;              ///< Show X number of log entries on website. Be careful, Max 1024 bits can be passed during a Website Refresh/Load event
 static const uint8_t QueueDepth = 32;           ///< Limits the maximum number of components within a module. Memory intense!
 static const uint8_t MovingAverageDepth = 10;   ///< Number of previous readings to keep when calculating average. Memory intense!
