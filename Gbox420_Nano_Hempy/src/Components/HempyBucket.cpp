eb062014cf3b8442ae98008ea956b1c57c949387
GrowBoxGuy <growboxguy@gmail.com>
2026-05-19 19:37:01 +0200

    Fi weight not updating during WATERING

--- Gbox420_Nano_Hempy/src/Components/HempyBucket.cpp
+++ Gbox420_Nano_Hempy/src/Components/HempyBucket.cpp
@@ -92,7 +92,7 @@ void HempyBucket::updateState(HempyStates NewState)
   while (ProcessUpdate)
   {
     ProcessUpdate = false;                        ///< Ensure the update is processed only once, even if the state changes multiple times during the function executionBucketWeightSensor.readWeight();              ///< Force Bucket weight update
-    StateWeight = BucketWeightSensor.getWeight(); // Store the bucket weight before
+    StateWeight = BucketWeightSensor.readWeight(); // Store the bucket weight before
     bool ChangeDetected = (State != TargetState); ///< Detect if the state is changing
 
     if (ChangeDetected)
